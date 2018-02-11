ada4c0dd4017c68a88ce51527b949394c972ace2
xtreak <tir.karthi@gmail.com>
2018-02-11 13:19:06 +0530

    Bump to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xtreak/numcompress "0.1.0"
+(defproject xtreak/numcompress "0.1.1-SNAPSHOT"
   :description "A clojure port of numcompress"
   :url "http://github.com/tirkarthi/numcompress"
   :license {:name "MIT License"
