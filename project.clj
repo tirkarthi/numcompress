e2136f034bc6d4bc42dac618f66e2a186dfdaaee
xtreak <tir.karthi@gmail.com>
2018-04-19 15:27:02 +0530

    Add codox for docs

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/tirkarthi/numcompress"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox {:output-path "codox"})
