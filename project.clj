94c323c4cb95ca9c27dca94b36b6c8d51eec43b1
xtreak <tir.karthi@gmail.com>
2018-04-19 15:28:14 +0530

    Initial commit to gh-pages

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject xtreak/numcompress "0.1.1-SNAPSHOT"
-  :description "A clojure port of numcompress"
-  :url "http://github.com/tirkarthi/numcompress"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [[lein-codox "0.10.3"]]
-  :codox {:output-path "codox"})
--- src/numcompress/utils.clj
+++
@@ -1,7 +0,0 @@
-(ns numcompress.utils)
-
-(defmacro validate-spec
-  [spec input]
-  `(if-not (spec/valid? ~spec ~input)
-     (throw (Exception. (spec/explain-str ~spec ~input)))
-     true))
